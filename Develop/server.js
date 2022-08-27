d2c83f164ef29bf28d05b34a2acc2b935f982fde
Luan Pham <luanpham@Luans-MacBook-Pro.local>
2022-08-27 10:33:59 -0700

    added GET requests for / and /notes URL to link to html pages

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const app = express();
 const PORT = 3001;
 
-app.listen(PORT);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
