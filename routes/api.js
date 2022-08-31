9163c0681786421ac234790b074b7753f05d42ac
unknown <phamexb@gmail.com>
2022-08-30 23:04:52 -0700

    working get and post requests

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,55 @@
 const apiRouter = require("express").Router();
 const fs = require("fs");
 const path = require("path");
+const util = require("util");
 
-apiRouter.get("/notes", (req, res) => {
-  console.info(`${req.method} request received for notes`);
-  readFromFile("./db.json").then((data) => res.json(JSON.parse(data)));
+const readFileaSync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const randomID = function () {
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
+
+apiRouter.get("/notes", function (req, res) {
+  readFileaSync("./db/db.json", "utf-8").then(function (data) {
+    notes = [].concat(JSON.parse(data));
+    res.json(notes);
+  });
 });
 
-apiRouter.post("/notes", (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-  console.log(req.body);
+apiRouter.post("/notes", function (req, res) {
+  const note = req.body;
+  readFileaSync("./db/db.json", "utf-8")
+    .then(function (data) {
+      const notes = [].concat(JSON.parse(data));
+      notes.id = randomID();
+      notes.push(note);
+      return notes;
+    })
+    .then(function (notes) {
+      writeFileAsync("./db/db.json", JSON.stringify(notes));
+      res.json(note);
+    });
+});
 
-  const { title, text } = req.body;
-
-  if (req.body) {
-    const newNote = {
-      title,
-      text,
-    };
-
-    readAndAppend(newNote, "./db/db.json");
-    res.json(`Note added successfully 🚀`);
-  } else {
-    res.error("Error in adding note");
-  }
+apiRouter.delete("/notes/:id", function (req, res) {
+  const noteID = parseInt(req.params.id);
+  readFileaSync("./db/db.json", "utf-8")
+    .then(function (data) {
+      const notes = [].concat(JSON.parse(data));
+      const notesData = [];
+      for (let i = 0; i < notes.length; i++) {
+        if (noteID !== notes[i].id) {
+          notesData.push(notes[i]);
+        }
+      }
+      return notesData;
+    })
+    .then(function (notes) {
+      writeFileAsync("./db/db.json", JSON.stringify(notes));
+    });
 });
 
 module.exports = apiRouter;
